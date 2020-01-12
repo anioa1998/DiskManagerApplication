4fcc03ab882c72daf025910df83236be7a5a8a2e
anioa1998 <anioa1998@wp.pl>
2020-01-13 00:21:23 +0100

    ShowDiskInformation almost done - menu return to do

--- DiskManagerApplication/DriveOperations/DiskPartitionInformation.cs
+++ DiskManagerApplication/DriveOperations/DiskPartitionInformation.cs
@@ -4,14 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management;
+using System.Collections;
+
 namespace DiskManagerApplication
 {
     public class DiskPartitionInformation
     {
         ManagementScope scope { get; set; }
+        ObjectQuery query { get; set; }
+        ManagementObjectSearcher searcher { get; set; }
+        ManagementObjectCollection queryCollection { get; set; }
+
         public DiskPartitionInformation(ManagementScope scope)
         {
             this.scope = scope;
+        }
+
+        public void ShowDiskInformation()
+        {
+            double diskSizeGB;
+
+            try
+            {
+                query = new ObjectQuery("SELECT * FROM Win32_DiskDrive");
+                if (query == null)
+                    throw new Exception("Nie wykonano zapytania - ObjectQuery Failed");
+
+                searcher = new ManagementObjectSearcher(scope, query);
+                if (searcher == null)
+                    throw new Exception("Nie znaleziono obiektów spełniających zapytanie - ManagementObjectSearcher Failed");
+
+                queryCollection = searcher.Get();
+
+                foreach(ManagementObject diskData in queryCollection)
+                {
+                    diskSizeGB = Convert.ToInt64(diskData["Size"]) / 1000000;
+
+                    Console.WriteLine("---------------------------------------------");
+                    Console.WriteLine("Nazwa dysku: {0}", diskData["Name"]);
+                    Console.WriteLine("Model: {0}", diskData["Model"]);
+                    Console.WriteLine("Rozmiar: {0} GB", diskSizeGB);
+                    Console.WriteLine("Numer seryjny {0}", diskData["SerialNumber"]);
+                    Console.WriteLine("Całkowita ilość sektorów: {0}", diskData["TotalSectors"]);
+                    Console.WriteLine("Status: {0}", diskData["Status"]);
+                    Console.WriteLine("Typ interfejsu: {0}", diskData["InterfaceType"]);
+                    Console.WriteLine("Rodzaj pamięci: {0}", diskData["MediaType"]);
+                    Console.WriteLine("Rewizja Firmware: {0}", diskData["FirmwareRevision"]);
+                    Console.WriteLine("Opis zdolności:");
+                    IEnumerable enumerable = diskData["CapabilityDescriptions"] as IEnumerable;
+                    if (enumerable != null)
+                    {
+                        foreach (object element in enumerable)
+                        {
+                            Console.WriteLine("- " + element);
+                        }
+                    }
+                    Console.WriteLine("---------------------------------------------\n");
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("DiskPartitionInformation Error: " + e.Message);
+            }
+        }
+
+        public void ShowPartitionInformation()
+        {
+
         }
     }
 }
--- DiskManagerApplication/Services/MenuService.cs
+++ DiskManagerApplication/Services/MenuService.cs
@@ -19,16 +19,61 @@ namespace DiskManagerApplication
         public void MainMenu()
         {
             int mainMenuSelect;
+            bool switchExit = true;
             Console.WriteLine("Witaj w menu głównym DiskManagerApplication. Wybierz opcję:");
 
             MainMenuOptions();
 
-            Console.Write("Wybieram: ");
+            
             mainMenuSelect = Convert.ToInt32(Console.ReadLine());
+            
+            do
+            {
+                Console.Clear();
+                switchExit = true;
 
-            Console.Clear();
+                switch (mainMenuSelect)
+                {
+                    case 0:
+                        {
+                            break;
+                        }
+                    case 1:
+                        {
+                            DiskPartitionInformationMenu();
+                            break;
+                        }
+                    case 2:
+                        {
 
-            switch (mainMenuSelect)
+                            break;
+                        }
+                    case 3:
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Wybrana opcja nie istnieje. Wybierz ponownie.\n");
+                            MainMenuOptions();
+                            mainMenuSelect = Convert.ToInt32(Console.ReadLine());
+                            switchExit = false;
+                            break;
+                        }
+                }
+            } while (!switchExit);
+        }
+
+        private void DiskPartitionInformationMenu()
+        {
+            DiskPartitionInformation diskPartitionInformation = new DiskPartitionInformation(scope);
+            int diskInformationSelect;
+
+            DiskPartitionInformationMenuOptions();
+
+            diskInformationSelect = Convert.ToInt32(Console.ReadLine());
+
+            switch(diskInformationSelect)
             {
                 case 0:
                     {
@@ -36,24 +81,41 @@ namespace DiskManagerApplication
                     }
                 case 1:
                     {
-                        DiskPartitionInformationMenu();   
+                        diskPartitionInformation.ShowDiskInformation();
                         break;
                     }
                 case 2:
                     {
-                        
-                        break;
-                    }
-                case 3:
-                    {
+                        diskPartitionInformation.ShowPartitionInformation();
                         break;
                     }
                 default:
                     {
-
                         break;
                     }
             }
+
+            Console.Clear();
+        }
+
+        private void MainMenuOptions()
+        {
+            Console.WriteLine("[1] - Parametry dysków HDD, SSD, USB i ich partycji");
+            Console.WriteLine("[2] - Dane S.M.A.R.T. *Obsługuje prawidłowo jedynie dyski HDD");
+            Console.WriteLine("[3] - Formatowanie wybranej partycji");
+            Console.WriteLine("[0] - Wyjście z programu");
+            Console.Write("Wybieram: ");
+        }
+       
+        private void DiskPartitionInformationMenuOptions()
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Parametry dysków i partycji");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("[1] - Wyświetl parametry dysków"); //Automatyczne generowanie raportu .txt
+            Console.WriteLine("[2] - Wyświetl parametry partycji");
+            Console.WriteLine("[0] - Wyjście z programu");
+            Console.Write("Wybieram: ");
         }
 
         public void ExitMessage()
@@ -64,34 +126,6 @@ namespace DiskManagerApplication
             Console.WriteLine("---------------------------------------------\n");
             Console.WriteLine("Wciśnij dowolny klawisz, aby zakończyć");
             Console.ReadKey();
-        }
-
-        private void MainMenuOptions()
-        {
-            Console.WriteLine("[1] - Parametry dysków i partycji");
-            Console.WriteLine("[2] - Dane S.M.A.R.T. *Obsługuje prawidłowo jedynie dyski HDD");
-            Console.WriteLine("[3] - Formatowanie wybranej partycji");
-            Console.WriteLine("[0] - Wyjście z programu");
-        }
-        private void DiskPartitionInformationMenu()
-        {
-            int diskInformationSelect;
-
-            DiskPartitionInformationMenuOptions();
-            Console.Write("Wybieram: ");
-
-            diskInformationSelect = Convert.ToInt32(Console.ReadLine());
-
-            Console.Clear();
-        }
-        private void DiskPartitionInformationMenuOptions()
-        {
-            Console.WriteLine("---------------------------------------------");
-            Console.WriteLine("Parametry dysków i partycji");
-            Console.WriteLine("---------------------------------------------");
-            Console.WriteLine("[1] - Wyświetl parametry dysków"); //Automatyczne generowanie raportu .txt
-            Console.WriteLine("[2] - Wyświetl parametry partycji");
-            Console.WriteLine("[0] - Wyjście z programu");
         }
     }
 }
