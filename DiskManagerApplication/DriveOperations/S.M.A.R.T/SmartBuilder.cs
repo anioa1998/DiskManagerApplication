8e0033e2a5190fb1255ed6aa956cb38adcf7bf5c
anioa1998 <anioa1998@wp.pl>
2020-01-27 20:30:59 +0100

    Working on Smart

---
+++ DiskManagerApplication/DriveOperations/S.M.A.R.T/SmartBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskManagerApplication.DriveOperations.S.M.A.R.T
+{
+    public class SmartBuilder : ISmartBuilder
+    {
+        private ManagementScope scope;
+        private ManagementObjectSearcher searcher;
+        private Dictionary<int, HardDriveData> allDrivesDictionary;
+
+        public void SetScope(ManagementScope scope)
+        {
+            this.scope = scope;
+        }
+        public void SetDriveStorage()
+        {
+            allDrivesDictionary = new Dictionary<int, HardDriveData>();
+        }
+        public void Build()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
